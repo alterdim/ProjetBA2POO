4e43a92ec167a66edfc0572f045f8f80efb07515
Alternis <roilouisdim@gmail.com>
2021-02-23 13:07:24 +0100

    javadoc Trip

--- src/ch/epfl/tchu/game/Trip.java
+++ src/ch/epfl/tchu/game/Trip.java
@@ -18,6 +18,11 @@ public final class Trip {
     private final Station to;
     private final int points;
 
+    /** Constructeur de Trip, un voyage qui part d'une station vers une autre et rapporte des points une fois complété.
+     * @param from Station de départ.
+     * @param to Station d'arrivée.
+     * @param points Nombre de points accordés.
+     */
     public Trip(Station from, Station to, int points) {
         Preconditions.checkArgument(points>0);
         this.from= Objects.requireNonNull(from);
@@ -25,6 +30,12 @@ public final class Trip {
         this.points=points;
     }
 
+    /**
+     * @param from Stations de départ.
+     * @param to Stations d'arrivée.
+     * @param points Points accordés à chaque voyage.
+     * @return Renvoie une liste de tous les voyages directs entre deux listes de stations.
+     */
     public static List<Trip> all(List<Station> from, List<Station> to, int points){
         Preconditions.checkArgument(!from.isEmpty());
         Preconditions.checkArgument(!to.isEmpty());
@@ -39,18 +50,31 @@ public final class Trip {
         return trips;
     }
 
+    /**
+     * @return Renvoie la station de départ d'un voyage.
+     */
     public Station from(){
         return this.from;
     }
 
+    /**
+     * @return Renvoie la station d'arrivée d'un voyage.
+     */
     public Station to(){
         return this.to;
     }
 
+    /**
+     * @return Renvoie le nombre de points d'un voyage.
+     */
     public int points(){
         return this.points;
     }
 
+    /**
+     * @param stationConnectivity La connectivité d'un joueur.
+     * @return Renvoie le nombre de points calculés d'un voyage (le nombre de points si rempli, l'opposé sinon).
+     */
     public int points(StationConnectivity stationConnectivity){
         return stationConnectivity.connected(from, to) ? this.points : -this.points;
     }
