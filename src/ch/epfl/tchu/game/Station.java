18bdbe7d9ee56ddb064c926c635e00506f978390
Alternis <roilouisdim@gmail.com>
2021-02-23 13:01:05 +0100

    javadoc Station

--- src/ch/epfl/tchu/game/Station.java
+++ src/ch/epfl/tchu/game/Station.java
@@ -13,21 +13,34 @@ public final class Station {
     private final int id;
     private final String name;
 
+    /** Constructeur de Stations.
+     * @param id Le numéro de la station. DOIT être unique.
+     * @param name Le nom de la station. Peut avoir des doublons pour les gares de pays.
+     */
     public Station(int id, String name) {
         Preconditions.checkArgument(id >= 0);
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * @return Renvoie le nom de la Station. Préférer name() si possible.
+     */
     @Override
     public String toString() {
         return name;
     }
 
+    /**
+     * @return Renvoie l'identification unique de la Station.
+     */
     public int id() {
         return id;
     }
 
+    /**
+     * @return Renvoie le nom de la Station.
+     */
     public String name() {
         return name;
     }
