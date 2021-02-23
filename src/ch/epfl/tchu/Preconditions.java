007a20764b5e05e26fd87f6b2982b64a688356ec
cemuelle <celien.muller@epfl.ch>
2021-02-23 13:35:04 +0100

    JavaDoc

--- src/ch/epfl/tchu/Preconditions.java
+++ src/ch/epfl/tchu/Preconditions.java
@@ -2,15 +2,23 @@ package ch.epfl.tchu;
 
 /**
  *
+ * Lève des exceptions si les conditions ne sont pas satisfaites.
+ *
  * @author Louis Gerard (296782)
  * @author Célien Muller (310777)
  */
-
 public final class Preconditions {
-    private Object IllegalArgumentException;
 
+    /**
+     * Constructeur par défaut privé pour ne pas être instancié
+     */
     private Preconditions() {}
 
+    /**
+     * Lève une excpetion
+     * @param shouldBeTrue paramètre controler
+     * @throws IllegalArgumentException si la condition n'est pas satisfaite
+     */
     public static void checkArgument(boolean shouldBeTrue) {
         if (!shouldBeTrue) {
             throw new IllegalArgumentException();
