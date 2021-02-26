f4df41ca1d5dad79d946bc30606ab7105f10d17a
Alternis <roilouisdim@gmail.com>
2021-02-26 16:14:11 +0100

    claimpoints + javadoc

--- src/ch/epfl/tchu/game/Route.java
+++ src/ch/epfl/tchu/game/Route.java
@@ -188,9 +188,26 @@ public final class Route {
 
     }
 
+    /**
+     * @return retourne le nombre de points appropriés vis-à-vis de la longueur de la route.
+     */
     public int claimPoints(){
-        //TODO ajouter méthode
-        return 0;
+       switch (length) {
+           case 1:
+               return 1;
+           case 2:
+               return 2;
+           case 3:
+               return 4;
+           case 4:
+               return 7;
+           case 5:
+               return 10;
+           case 6:
+               return 15;
+           default:
+               return 0;
+       }
     }
 }
 
