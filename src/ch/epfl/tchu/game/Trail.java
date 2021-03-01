50fd557c68c6da3544e76c06b301e2fbb2ca9c85
cemuelle <celien.muller@epfl.ch>
2021-03-01 09:37:13 +0100

    Trail almost finish

--- src/ch/epfl/tchu/game/Trail.java
+++ src/ch/epfl/tchu/game/Trail.java
@@ -2,6 +2,7 @@ package ch.epfl.tchu.game;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,22 +13,71 @@ import java.util.List;
  */
 public final class Trail {
     private final List<Route> routes;
+    private final Station stationFrom;
+    private final Station stationTo;
 
-    private Trail(List<Route> routes) {
-        this.routes = routes;
+    private final int length;
+
+
+
+    private Trail(Station stationFrom, Station stationTo, List<Route> routes, int length) {
+        this.stationFrom=stationFrom;
+        this.stationTo=stationTo;
+        this.routes=routes;
+        this.length=length;
     }
 
+    public static Trail longest(List<Route> routes){
+        if (routes.isEmpty()) return new Trail(null, null, null, 0);
+
+
+        List<Trail> cs = new ArrayList<>();
+
+        for (Route route:routes){
+            cs.add(new Trail(route.station1(), route.station2(), List.of(route), route.length()));
+            cs.add(new Trail(route.station2(), route.station1(), List.of(route), route.length()));
+        }
+
+        while (!cs.isEmpty()){
+            List<Trail> cs2 = new ArrayList<>();
+            for(Trail c:cs){
+
+                List<Route> rs= new ArrayList<>(routes);
+
+                rs.removeAll(c.routes);
+                rs.removeIf(r -> !r.stations().contains(c.stationTo));
+
+                for (Route r : rs){
+                    List<Route> routeList = new ArrayList<>(c.routes);
+                    routeList.add(r);
+                    cs2.add(new Trail(c.stationFrom, r.stationOpposite(c.stationTo), routeList, c.length+r.length()));
+                }
+
+            }
+            if(cs2.isEmpty()){
+                return cs.get(0);
+            }
+            cs=cs2;
+        }
+        return null;
+    }
+
+
     public Station station1() {
-        return routes.get(0).station1();
+        return ((length()!=0) ? stationFrom: null);
     }
 
     public Station station2() {
-        return routes.get(0).station2();
+        return ((length()!=0) ? stationTo: null);
     }
 
-    public static Trail longest(List<Route> routes) {
-        //TODO
-        return null;
+    public int length(){
+        return length;
+    }
 
+    @Override
+    public String toString() {
+        //TODO
+        return stationFrom.name()+" - "+stationTo.name();
     }
 }
