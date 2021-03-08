ebd11c673a80c5cb2142d7639252beba82047cc8
cemuelle <celien.muller@epfl.ch>
2021-03-08 15:28:27 +0100

    playerState middle

---
+++ src/ch/epfl/tchu/game/PlayerState.java
@@ -0,0 +1,101 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.Preconditions;
+import ch.epfl.tchu.SortedBag;
+
+import java.util.List;
+
+/**
+ * Créé le 08.03.2021 à 14:02
+ *
+ * @author Louis Gerard (296782)
+ * @author Célien Muller (310777)
+ */
+public final class PlayerState extends PublicPlayerState {
+    private final SortedBag<Ticket> tickets;
+    private final  SortedBag<Card> cards;
+
+
+    /**
+     * Construit un état de joueur
+     * @param tickets tickets du joueur
+     * @param cards cartes du joueur
+     * @param routes routes appartenant au joueur
+     */
+    public PlayerState(SortedBag<Ticket> tickets, SortedBag<Card> cards, List<Route> routes) {
+        super(tickets.size(), cards.size(), routes);
+        this.tickets=tickets;
+        this.cards=cards;
+    }
+
+    /**
+     * Initialise un état de joueur avec uniquement les cartes initiales distribuées, mais sans route ni tickets
+     * @param initialCards les cartes initiales données
+     * @return l'état initial d'un joueur
+     * @throws IllegalArgumentException si le nombre de cartes initiales ne vaut pas 4
+     */
+    public static PlayerState initial(SortedBag<Card> initialCards){
+        Preconditions.checkArgument(initialCards.size()==4);
+        return new PlayerState(new SortedBag.Builder<Ticket>().build(), initialCards, List.of());
+    }
+
+    /**
+     *
+     * @return les billets du joueur
+     */
+    public SortedBag<Ticket> tickets(){
+        return tickets;
+    }
+
+    /**
+     * Ajoute des billets
+     * @param newTickets des billets
+     * @return Un nouvel état du joueur avec des billets en plus
+     */
+    public PlayerState withAddedTickets(SortedBag<Ticket> newTickets){
+        return new PlayerState(newTickets.union(tickets()), cards(), routes());
+    }
+
+    /**
+     * @return  les cartes wagon/locomotive du joueur
+     */
+    public SortedBag<Card> cards(){
+        return cards;
+    }
+
+    /**
+     * Ajoute une carte a l'état du joueur
+     * @param card Une carte
+     * @return Un nouvel état du joueur avec une carte en plus
+     */
+    public PlayerState withAddedCard(Card card){
+        return new PlayerState(tickets(), new SortedBag.Builder<Card>().add(cards()).add(card).build(), routes());
+    }
+
+    /**
+     * Ajoute des cartes
+     * @param additionalCards des cartes
+     * @return Un nouvel état du joueur avec des cartes en plus
+     */
+    public PlayerState withAddedCards(SortedBag<Card> additionalCards){
+        return new PlayerState(tickets(), additionalCards.union(cards()), routes());
+    }
+
+    /**
+     * Le joueur possède-t-il les wagons et les cartes pour s'emparer de la route
+     * @param route une route
+     * @return vrai s'il peut s'en emparer
+     */
+    public boolean canClaimRoute(Route route){
+        /*int routeLength= route.length();
+        //Nombre de wagon >= à la longueur de la route et (||)
+        route.possibleClaimCards();
+//        cards.
+
+        if (routeLength<=carCount() *//*|| routeLength<=route.color()*//*){
+            return true;
+        }
+        else return false;*/
+        return true;
+    }
+}
---
+++ test/ch/epfl/tchu/game/PlayerStateTest.java
@@ -0,0 +1,96 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.SortedBag;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Créé le 08.03.2021 à 15:08
+ *
+ * @author Louis Gerard (296782)
+ * @author Célien Muller (310777)
+ */
+public class PlayerStateTest {
+    @Test
+    void WorkwithAddedTickets(){
+        var map = new TestMap();
+        var cards = new SortedBag.Builder<Card>()
+                .add(Card.BLACK)
+                .add(Card.BLUE)
+                .add(Card.RED)
+                .add(Card.LOCOMOTIVE)
+
+                .build();
+        var playerState = PlayerState.initial(cards);
+        var tickets = new SortedBag.Builder<Ticket>()
+                .add(map.LAU_BER)
+                .add(map.BER_NEIGHBORS)
+                .add(map.LAU_STG)
+                .add(map.FR_NEIGHBORS)
+                .build();
+        assertEquals(tickets, playerState.withAddedTickets(tickets).tickets());
+    }
+
+    @Test
+    void WorkwithAddedCard(){
+        var map = new TestMap();
+        var cards = new SortedBag.Builder<Card>()
+                .add(Card.BLACK)
+                .add(Card.BLUE)
+                .add(Card.RED)
+                .add(Card.LOCOMOTIVE)
+
+                .build();
+        var playerState = PlayerState.initial(cards);
+
+        var cards2 = new SortedBag.Builder<Card>()
+                .add(cards)
+                .add(Card.GREEN)
+                .build();
+
+        assertEquals(cards2, playerState.withAddedCard(Card.GREEN).tickets());
+    }
+
+    private static final class TestMap {
+        // Stations - cities
+        public final Station BER = new Station(0, "Berne");
+        public final Station LAU = new Station(1, "Lausanne");
+        public final Station STG = new Station(2, "Saint-Gall");
+
+        // Stations - countries
+        public final Station DE1 = new Station(3, "Allemagne");
+        public final Station DE2 = new Station(4, "Allemagne");
+        public final Station DE3 = new Station(5, "Allemagne");
+        public final Station AT1 = new Station(6, "Autriche");
+        public final Station AT2 = new Station(7, "Autriche");
+        public final Station IT1 = new Station(8, "Italie");
+        public final Station IT2 = new Station(9, "Italie");
+        public final Station IT3 = new Station(10, "Italie");
+        public final Station FR1 = new Station(11, "France");
+        public final Station FR2 = new Station(12, "France");
+
+        // Countries
+        public final List<Station> DE = List.of(DE1, DE2, DE3);
+        public final List<Station> AT = List.of(AT1, AT2);
+        public final List<Station> IT = List.of(IT1, IT2, IT3);
+        public final List<Station> FR = List.of(FR1, FR2);
+
+        public final Ticket LAU_STG = new Ticket(LAU, STG, 13);
+        public final Ticket LAU_BER = new Ticket(LAU, BER, 2);
+        public final Ticket BER_NEIGHBORS = ticketToNeighbors(List.of(BER), 6, 11, 8, 5);
+        public final Ticket FR_NEIGHBORS = ticketToNeighbors(FR, 5, 14, 11, 0);
+
+        private Ticket ticketToNeighbors(List<Station> from, int de, int at, int it, int fr) {
+            var trips = new ArrayList<Trip>();
+            if (de != 0) trips.addAll(Trip.all(from, DE, de));
+            if (at != 0) trips.addAll(Trip.all(from, AT, at));
+            if (it != 0) trips.addAll(Trip.all(from, IT, it));
+            if (fr != 0) trips.addAll(Trip.all(from, FR, fr));
+            return new Ticket(trips);
+        }
+    }
+}
