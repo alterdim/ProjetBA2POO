1803c076053fa37c6cf33219336f1aba9a5ee865
cemuelle <celien.muller@epfl.ch>
2021-03-09 18:41:12 +0100

    small fix

--- src/ch/epfl/tchu/gui/Info.java
+++ src/ch/epfl/tchu/gui/Info.java
@@ -12,28 +12,30 @@ import java.util.List;
 import static ch.epfl.tchu.gui.StringsFr.*;
 
 /**
+ * Générations des messages des actions des joueurs
+ *
  * Fichier créé à 14:34 le 02/03/2021
  *
  * @author Louis Gerard (296782)
  * @author Célien Muller (310777)
  */
-public class Info {
+public final class Info {
     private final String playerName;
 
+    /**
+     * Lie les messages au joueur
+     * @param playerName Nom du joueur
+     */
     public Info(String playerName) {
         this.playerName = playerName;
-
     }
 
     /**
      * @param card Une carte
      * @param count Le nombre de cartes. Doit être supérieur à 0.
-     * @throws IllegalArgumentException si la carte demandée n'a pas de texte français associé (ne devrait jamais arriver),
-     * ou si le nombre de cartes n'est pas strictement positif.
      * @return un string en français lié à la couleur et à la multiplicité d'une carte.
      */
     public static String cardName(Card card, int count) {
-        Preconditions.checkArgument(count > 0);
         StringBuilder colorString=new StringBuilder();
         switch (card) {
             case BLACK:
@@ -63,9 +65,6 @@ public class Info {
             case LOCOMOTIVE:
                 colorString.append(LOCOMOTIVE_CARD);
                 break;
-            default:
-                colorString.append("somethingIsWrong !");
-                throw new IllegalArgumentException();
         }
         colorString.append(plural(count));
         return colorString.toString();
--- test/ch/epfl/tchu/gui/InfoTest.java
+++ test/ch/epfl/tchu/gui/InfoTest.java
@@ -5,7 +5,11 @@ import ch.epfl.tchu.game.Card;
 import ch.epfl.tchu.game.Color;
 import ch.epfl.tchu.game.Route;
 import ch.epfl.tchu.game.Station;
+import ch.epfl.test.TestRandomizer;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,19 +22,57 @@ import static org.junit.jupiter.api.Assertions.*;
 class InfoTest {
 
     @Test
+    void workInfo(){
+        new Info("Test");
+    }
+
+    @Test
     void cardName() {
+        assertEquals("noire", Info.cardName(Card.BLACK, 1));
+        assertEquals("noires", Info.cardName(Card.BLACK, 2));
+        assertEquals("noires", Info.cardName(Card.BLACK, 0));
+        assertEquals("noires", Info.cardName(Card.BLACK, 10));
+    }
+
+    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private static String randomName(Random rng, int length) {
+        var sb = new StringBuilder();
+        for (int i = 0; i < length; i++)
+            sb.append(alphabet.charAt(rng.nextInt(alphabet.length())));
+        return sb.toString();
     }
 
     @Test
     void draw() {
+        var rng = TestRandomizer.newRandom();
+        for (int i = 0; i < TestRandomizer.RANDOM_ITERATIONS; i++) {
+            var name1 = randomName(rng, 1 + rng.nextInt(10));
+            var name2 = randomName(rng, 1 + rng.nextInt(15));
+            var points = rng.nextInt(15);
+
+            assertEquals("\n"+name1+" et "+name2+" sont ex æqo avec "+points+" points !\n", Info.draw(List.of(name1, name2), points));
+        }
+
     }
 
     @Test
     void willPlayFirst() {
+        var rng = TestRandomizer.newRandom();
+        for (int i = 0; i < TestRandomizer.RANDOM_ITERATIONS; i++) {
+            var name = randomName(rng, 1 + rng.nextInt(10));
+
+            assertEquals(name+" jouera en premier.\n\n", new Info(name).willPlayFirst());
+        }
     }
 
     @Test
     void keptTickets() {
+        var rng = TestRandomizer.newRandom();
+        for (int i = 0; i < TestRandomizer.RANDOM_ITERATIONS; i++) {
+            var name = randomName(rng, 1 + rng.nextInt(10));
+            var number = rng.nextInt(15);
+            assertEquals(name+" a gardé "+ number +" billet"+StringsFr.plural(number) +".\n", new Info(name).keptTickets(number));
+        }
     }
 
     @Test
@@ -63,10 +105,35 @@ class InfoTest {
 
     @Test
     void attemptsTunnelClaim() {
+        //TODO
+        var rng = TestRandomizer.newRandom();
+        for (int i = 0; i < TestRandomizer.RANDOM_ITERATIONS; i++) {
+            var name = randomName(rng, 1 + rng.nextInt(10));
+            var number = rng.nextInt(15);
+            var cards = new SortedBag.Builder<Card>()
+                    .add(Card.BLACK)
+                    .add(Card.BLUE)
+                    .add(Card.RED)
+                    .add(Card.LOCOMOTIVE)
+                    .build();
+            assertEquals(name+" a gardé "+ number +" billet"+StringsFr.plural(number) +".\n", new Info(name).keptTickets(number));
+        }
     }
 
     @Test
     void drewAdditionalCards() {
+        var rng = TestRandomizer.newRandom();
+        for (int i = 0; i < TestRandomizer.RANDOM_ITERATIONS; i++) {
+            var name = randomName(rng, 1 + rng.nextInt(10));
+            var number = rng.nextInt(15);
+            var cards = new SortedBag.Builder<Card>()
+                    .add(Card.BLACK)
+                    .add(Card.BLUE)
+                    .add(Card.RED)
+                    .add(Card.LOCOMOTIVE)
+                    .build();
+            assertEquals("Les cartes supplémentaires sont "+cards.toString()+". ", new Info(name).drewAdditionalCards(cards, 0));
+        }
     }
 
     @Test
@@ -87,6 +154,7 @@ class InfoTest {
 
     @Test
     void generateCardString() {
+        //TODO
         Station station1 = new Station(1, "station1");
         Station station2 = new Station(2, "station2");
         Route route = new Route("route", station1, station2, 3, Route.Level.OVERGROUND, Color.BLACK);
