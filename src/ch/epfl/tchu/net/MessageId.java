58dc7891aaee2165bfed9c0483f19ca17e921cfb
cemuelle <celien.muller@epfl.ch>
2021-04-12 15:01:05 +0200

    MessageId

---
+++ src/ch/epfl/tchu/net/MessageId.java
@@ -0,0 +1,21 @@
+package ch.epfl.tchu.net;
+
+/**
+ * Créé le 12.04.2021 à 14:55
+ *
+ * @author Louis Gerard (296782)
+ * @author Célien Muller (310777)
+ */
+public enum MessageId {
+    INIT_PLAYERS,
+    RECEIVE_INFO,
+    UPDATE_STATE,
+    SET_INITIAL_TICKETS,
+    CHOOSE_INITIAL_TICKETS,
+    NEXT_TURN,
+    CHOOSE_TICKETS,
+    DRAW_SLOT,
+    ROUTE,
+    CARDS,
+    CHOOSE_ADDITIONAL_CARDS
+}
