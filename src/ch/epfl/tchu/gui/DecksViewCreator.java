8530c2f6d95d403627d511bc29189fe1e848fa8e
cemuelle <celien.muller@epfl.ch>
2021-05-01 15:32:25 +0200

    corrections

--- src/ch/epfl/tchu/gui/DecksViewCreator.java
+++ src/ch/epfl/tchu/gui/DecksViewCreator.java
@@ -7,6 +7,8 @@ import ch.epfl.tchu.gui.ActionHandlers.*;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -15,6 +17,8 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+
+import java.awt.*;
 
 
 /**
@@ -70,12 +74,15 @@ abstract class DecksViewCreator {
             tempTextCount = new Text();
             tempTextCount.getStyleClass().add("count");
             tempTextCount.textProperty().bind(Bindings.convert(count));
-            tempStackPane.visibleProperty().bind(Bindings.greaterThan(count, 1));
+            tempTextCount.visibleProperty().bind(Bindings.greaterThan(count, 1));
 
 
             tempStackPane.getChildren().addAll(tempRectangleOutside, tempRectangleInside, tempRectangleImage, tempTextCount);
             canvas.getChildren().add(tempStackPane);
         }
+
+        ticketListView.setItems(observableGameState.tickets());
+
 
 
         return canvas;
@@ -96,6 +103,24 @@ abstract class DecksViewCreator {
         canvas.setId("card-pane");//TODO id correct?
         canvas.getStylesheets().add("decks.css");
         canvas.getStylesheets().add("colors.css");
+
+
+        ticketsButton.getStyleClass().add("gauged");
+        Group tempGroupTicket = new Group();
+        Rectangle tempRectangleBackgroundTicket = new Rectangle(50, 5);
+        Rectangle tempRectangleForegroundTicket = new Rectangle(50, 5);
+
+        tempRectangleBackgroundTicket.getStyleClass().add("background");
+        tempRectangleForegroundTicket.getStyleClass().add("foreground");
+
+        tempRectangleForegroundTicket.widthProperty().bind(observableGameState.leftTicketsPercentage().multiply(50).divide(100));
+        tempGroupTicket.getChildren().addAll(tempRectangleBackgroundTicket, tempRectangleForegroundTicket);
+        ticketsButton.setGraphic(tempGroupTicket);
+        ticketsButton.disableProperty().bind(ticketHandler.isNull());
+        ticketsButton.setOnMouseClicked((event -> ticketHandler.get().onDrawTickets()));
+        ticketsButton.setText(StringsFr.TICKETS);//TODO faut-il ajouter le texte comme ca?
+
+        canvas.getChildren().add(ticketsButton);
 
 
         for (int index : Constants.FACE_UP_CARD_SLOTS) {
@@ -140,30 +165,19 @@ abstract class DecksViewCreator {
         }
 
         cardsButton.getStyleClass().add("gauged");
-        Group tempGroup = new Group();
-        Rectangle tempRectangleBackground = new Rectangle(50, 5);
-        Rectangle tempRectangleForeground = new Rectangle(50, 5);
+        Group tempGroupCard = new Group();
+        Rectangle tempRectangleBackgroundCard = new Rectangle(50, 5);
+        Rectangle tempRectangleForegroundCard = new Rectangle(50, 5);
+        tempRectangleBackgroundCard.getStyleClass().add("background");
+        tempRectangleForegroundCard.getStyleClass().add("foreground");
 
-        tempRectangleForeground.widthProperty().bind(observableGameState.leftCardsPercentage().multiply(50).divide(100));
-        tempGroup.getChildren().addAll(tempRectangleBackground, tempRectangleForeground);
-        cardsButton.setGraphic(tempGroup);
+        tempRectangleForegroundCard.widthProperty().bind(observableGameState.leftCardsPercentage().multiply(50).divide(100));
+        tempGroupCard.getChildren().addAll(tempRectangleBackgroundCard, tempRectangleForegroundCard);
+        cardsButton.setGraphic(tempGroupCard);
         cardsButton.disableProperty().bind(cardHandler.isNull());
         cardsButton.setOnMouseClicked((event -> cardHandler.get().onDrawCard(Constants.DECK_SLOT)));
-
-
-        ticketsButton.getStyleClass().add("gauged");
-        tempGroup = new Group();
-        tempRectangleBackground = new Rectangle(50, 5);
-        tempRectangleForeground = new Rectangle(50, 5);
-        tempRectangleForeground.widthProperty().bind(observableGameState.leftTicketsPercentage().multiply(50).divide(100));
-
-        tempGroup.getChildren().addAll(tempRectangleBackground, tempRectangleForeground);
-        ticketsButton.setGraphic(tempGroup);
-        ticketsButton.disableProperty().bind(ticketHandler.isNull());
-        ticketsButton.setOnMouseClicked((event -> ticketHandler.get().onDrawTickets()));
-        ticketsButton.setText("Billets");//TODO faut-il ajouter le texte comme ca?
-
-        canvas.getChildren().addAll(cardsButton, ticketsButton);
+        cardsButton.setText(StringsFr.CARDS);
+        canvas.getChildren().add(cardsButton);
         return canvas;
     }
 }
